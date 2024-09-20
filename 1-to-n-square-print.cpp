502ff864bc315ac30cab8222bfdf534691afb66e
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-20 22:48:37 +0530

    Add files via upload

---
+++ 1-to-n-square-print.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int i,n;
+    cout<<"Enter the numberm: ";
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        cout<<i*i<<endl;
+    }
+}
